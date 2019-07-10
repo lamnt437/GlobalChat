f96d2f925117dd0467578924e027fc01de0629af
Lam Nguyen 943 <lam.nt943@gmail.com>
2019-07-10 22:12:09 +0700

    update login action in UsersController

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -42,12 +42,18 @@ class UsersController extends BaseController
         }
     }
 
-    public function login(){
-        if (isset($_POST['btn_login'])) {
+    public function login() {
+        $status = "OK";
+
+        if (isset($_COOKIE['chat_token']) && isset($_SESSION['chat_token'])) {            
+            if($_COOKIE['chat_token'] === $_SESSION['chat_token']) {
+                header("Location: index.php?controller=pages&action=home");
+            }
+        }
+
+        elseif (isset($_POST['btn_login'])) {
             $user_name = $_POST['user_name_lgin'];
             $user_pw = $_POST['user_pw_lgin'];
-
-            $status = "OK";
 
             if (
                 empty($user_name) ||
@@ -57,12 +63,16 @@ class UsersController extends BaseController
             } else {
                 $found = User::foundUser($user_name, $user_pw);
                 if($found){
+                    // die("HELLO");
                     $status = "OK";
-                    session_start();
                     $result = User::findByUsername($user_name);
-                    $_SESSION['user_name'] = $user_name;
-                    $_SESSION['user_id'] = $result['user_id'];
-                    $_SESSION['full_name'] = $result['full_name'];
+                    // $_SESSION['user_name'] = $user_name;
+                    // $_SESSION['user_id'] = $result['user_id'];
+                    // $_SESSION['full_name'] = $result['full_name'];
+                    $chat_token = uniqid();
+                    $_SESSION['chat_token'] = $chat_token;
+                    // $_COOKIE['chat_token'] = $chat_token;
+                    setcookie('chat_token', $chat_token, time() + (86400 * 30), "/");
                     header("Location: index.php?controller=pages&action=home");
                 }
                 else
@@ -71,8 +81,9 @@ class UsersController extends BaseController
 
             $data = array("status" => $status);
             $this->render('login', $data);
+        }
 
-        } else {
+        else {
             $data = array();
             $this->render('login', $data);
         }
@@ -80,7 +91,8 @@ class UsersController extends BaseController
 
     public function logout(){
         session_start();
-        session_destroy();
+        unset($_SESSION['chat_token']);
+        unset($_COOKIE['chat_token']);
         header("Location: index.php?controller=users&action=login");
     }
 }
